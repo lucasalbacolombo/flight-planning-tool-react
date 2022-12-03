8a68dfa8d83e69372fd23aee0c88da03a79e6aba
Lucas Colombo <colombo.lucas@outlook.com>
2022-12-03 17:45:30 -0300

    api production url

--- src/api/api.js
+++ src/api/api.js
@@ -1,22 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 
 const apiURLs = {
-	development: 'http://localhost:4000',
-	production: 'https://flight-planning-tool.herokuapp.com',
+  development: "http://localhost:4000",
+  production: "https://mushy-shrug-tuna.cyclic.app/",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
 
 api.interceptors.request.use((config) => {
-	const loggedInUserJSON = localStorage.getItem('loggedInUser');
+  const loggedInUserJSON = localStorage.getItem("loggedInUser");
 
-	const parseLoggedInUser = JSON.parse(loggedInUserJSON || '""');
+  const parseLoggedInUser = JSON.parse(loggedInUserJSON || '""');
 
-	if (parseLoggedInUser.token) {
-		config.headers = { Authorization: `Bearer ${parseLoggedInUser.token}` };
-	}
+  if (parseLoggedInUser.token) {
+    config.headers = { Authorization: `Bearer ${parseLoggedInUser.token}` };
+  }
 
-	return config;
+  return config;
 });
 
 export { api };
